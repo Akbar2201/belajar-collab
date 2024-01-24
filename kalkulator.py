494b906323e9f4c4fa4771c2b90cb1237b319acd
Akbar2201 <muhammadakbar220105@gmail.com>
2024-01-24 09:32:01 +0700

    menambahkan pembagian

--- kalkulator.py
+++ kalkulator.py
@@ -2,19 +2,16 @@ def tambah(a,b):
   return a + b
 def kurang(bil_a,bil_b) :
   return bil_a - bil_b
-<<<<<<< HEAD
 def perkalian(x,y) :
   return x * y
+def pembagian(a,b) :
+  return a / b
 
-=======
-def perkalian(angka1,angka2) :
-  return angka1 * angka2
-  
->>>>>>> 4d2d4553286121126ee8f09230a6b21a1d5eae48
 print("Pilih operasi :")
 print("1. Penjumlahan")
 print("2. Pengurangan")
 print("3. Perkalian")
+print("4. Pembagian")
 
 pilihan = input("Masukkan pilihan (1/2/3) : ")
 
@@ -28,5 +25,6 @@ if pilihan in ('1', '2', '3'):
     print(angka1, "-", angka2, "=", kurang(angka1, angka2))
   elif pilihan == '3':
     print(angka1, "*", angka2, "=", perkalian(angka1, angka2))
-  else
-    print("Pilihan tidak valid")
+  elif pilihan == '4':
+    print(angka1, "/", angka2, "=", pembagian(angka1, angka2))
+
